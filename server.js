02d395db8cdd67da72d6fe4b56f50ca6a7f1c818
John Hawley <jehawley@gmail.com>
2018-03-12 17:47:58 -0700

    first deploy

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+let app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+});
